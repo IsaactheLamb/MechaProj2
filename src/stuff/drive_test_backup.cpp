53b6b9d3226fef91b24cb45a8614da7a4d2bb476
Benjamiiiin <bensteer0@gmail.com>
2017-10-04 07:35:44 +0000

    Merging

--- src/stuff/drive_test_backup.cpp
+++ src/stuff/drive_test_backup.cpp
@@ -95,25 +95,19 @@ PI_THREAD (encThread)
     rpm_R = dirs[dir_R]*coeff*enc_cnt_R;
     
     //printf("Angular velocity: %.2f rad/s.", ang_vel);
-    //cout << "\tRPM Left  :" << (int) rpm_L << endl;
-    //cout << "\tRPM Right :" << (int) rpm_R << endl;
+    cout << "\tRPM Left  :" << (int) rpm_L << endl;
+    cout << "\t\tRPM Right :" << (int) rpm_R << endl;
   }
 }
-
-  // Previous angle
-  float prev_angle = 0;
-  float integral_angle = 0;
 
 PI_THREAD (imuThread)
 {  
   while(1)
   {
-	  prev_angle = angle; // Track previous angle for derivative term
-
 	  scanf("%f *", &angle);
-	  angle = (-1)*(angle - 0.05);
+	  angle = angle + 0.08;
 
-	  //cout << "\t\tAngle is " << angle << endl;
+	  cout << "\tAngle is " << angle << endl;
 
 	  // Read to end of line so don't get stuck on one invalid line.
 	  while(getc(stdin) != '\n');
@@ -205,37 +199,19 @@ int main(int argc, char **argv)
   //piThreadCreate (VisionThread); // Start Vision thread
   atexit(myExit);
 
-  double c1_p = 0.005;
+  double c1_p = 0.02;
   double c1_i = 0;
-  double c1_d = 0.002;
-  double Bp = 0;
-  double Bd = 0;
-  double Bi = 0;
-  double Bw = 0;
+  double c1_d = 0.05;
 
-  if (argc == 8)
+  if (argc == 4)
   {
     c1_p = stod(argv[1]);
     c1_i = stod(argv[2]);
     c1_d = stod(argv[3]);
-    Bp = stod(argv[4]);
-    Bi = stod(argv[5]);
-    Bd = stod(argv[6]);
-    Bw = stod(argv[7]);
-  }
-
-if (argc == 5)
-  {
-    Bp = stod(argv[1]);
-    Bi = stod(argv[2]);
-    Bd = stod(argv[3]);
-    Bw = stod(argv[4]);
   }
 
   MiniPID pid_L = MiniPID(c1_p, c1_i, c1_d);
   MiniPID pid_R = MiniPID(c1_p, c1_i, c1_d);
-  MiniPID pid_angle_L = MiniPID(Bp, Bi, Bd);
-  MiniPID pid_angle_R = MiniPID(Bp, Bi, Bd);
 
   float pwm_val_L = 0; // PWM values to write to pins
   float pwm_val_R = 0; 
@@ -254,9 +230,10 @@ if (argc == 5)
   for(;;) 
   {
     // *********************** IMU CONTROL ***********************
-    des_rpm = 0;
+    des_rpm = (angle < 0)*(-1)*sqrt(abs(angle))*MTR_MAX_RPM + (angle > 0)*sqrt(abs(angle))*MTR_MAX_RPM;
+    cout << "\tdesired RPM is " << des_rpm << endl;
 
-    // ******************** STEERING ********************
+    // ******************** DRIVE WHEEL PID CONTROL ********************
     bool force_dir_L = 0;
     bool force_dir_R = 0;
 
@@ -281,29 +258,14 @@ if (argc == 5)
 	force_dir_L = 1;
 	force_dir_R = 1;
     }
-    // ******************** DRIVE WHEEL PID CONTROL ********************
 
-    integral_angle += angle;
-
-    //float balance_out_L = Bp*angle - Bi*integral_angle - Bd*(angle - prev_angle) 
-	//	+ Bw*(des_rpm - rpm_L);
-    //float balance_out_R = Bp*angle - Bi*integral_angle - Bd*(angle - prev_angle) 
-	//	+ Bw*(des_rpm - rpm_R);
-
-    float balance_out_L  = pid_angle_L.getOutput(-angle, 0) + Bw*(des_rpm - rpm_L); 
-    float balance_out_R = pid_angle_R.getOutput(-angle, 0) + Bw*(des_rpm - rpm_R); 
-
-    //cout << "balance out L " << balance_out_L << endl;
-    //cout << "balance out R " << balance_out_R << endl;
-
-    //float balance_out_L = 50;
-    //float balance_out_R = 50;
-
-    double out_L = pid_L.getOutput(rpm_L, balance_out_L );     // Get delta in PWM
-    double out_R = pid_R.getOutput(rpm_R, balance_out_R );  // Get delta in PWM
+    double out_L = pid_L.getOutput(rpm_L, dirs[force_dir_L]*des_rpm); // Get delta in PWM
+    double out_R = pid_R.getOutput(rpm_R, dirs[force_dir_R]*des_rpm); // Get delta in PWM
 
     pwm_val_L += out_2_in*out_L; // Increment PWM
     pwm_val_R += out_2_in*out_R; 
+
+    
 
     // Set directions
     dir_L = pwm_val_L >= 0;
@@ -318,8 +280,8 @@ if (argc == 5)
     if (pwm_val_R > MAX_PWM) pwm_val_R = MAX_PWM;
     else if (pwm_val_R < -MAX_PWM) pwm_val_R = -MAX_PWM;
 
-   // cout << "WRITING TO PWMS *******************************************************" << endl;
-    if (abs(angle)>0.8)
+    cout << "WRITING TO PWMS *******************************************************" << endl;
+    if (abs(angle)>0.5)
 	{
 		pwm_val_L=0;
 		pwm_val_R=0;
